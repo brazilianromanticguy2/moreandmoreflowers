8dc8dfd9c92ff03a92f66c96d5bd096cedd19ef8
brazilianromanticguy2 <116916595+brazilianromanticguy2@users.noreply.github.com>
2022-10-28 16:43:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('starx').onclick = function () {
+  $('#cloudd').hide('fast');
+  $('#chestt').hide('fast');
+  $('#shipp').hide('fast');
+}
+
+document.getElementById('cloudx').onclick = function () {
+  $('#starr').hide('fast');
+  $('#chestt').hide('fast');
+  $('#shipp').hide('fast'); 
+}
+
+document.getElementById('chestx').onclick = function () {
+  $('#starr').hide('fast');
+  $('#cloudd').hide('fast');
+  $('#shipp').hide('fast'); 
+}
